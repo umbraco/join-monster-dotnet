1ebd8ee2b5e1648a78b4a0bebe2dfd269a726cbf
Rasmus John Pedersen <mail@rjp.dk>
2020-06-19 09:12:36 +0200

    Map type to array if Paginate is true

--- src/JoinMonster/ObjectShaper.cs
+++ src/JoinMonster/ObjectShaper.cs
@@ -54,7 +54,7 @@ namespace JoinMonster
                     case SqlTable sqlTable:
                         var childProperties = DefineObjectShape(node, prefix, sqlTable);
                         IProperty property;
-                        if (sqlTable.GrabMany)
+                        if (sqlTable.GrabMany || sqlTable.Paginate)
                             property = new PropertyArray(sqlTable.FieldName, childProperties);
                         else
                             property = new PropertyObject(sqlTable.FieldName, childProperties);
