78ea91f2e53487457056376b7745c9520d46ed9a
Rasmus John Pedersen <mail@rjp.dk>
2024-12-02 10:47:58 +0100

    fix: properties being added miltiple times in object shaping

--- src/JoinMonster/ObjectShaper.cs
+++ src/JoinMonster/ObjectShaper.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using JoinMonster.Language.AST;
 using NestHydration;
 
@@ -27,17 +29,17 @@ namespace JoinMonster
         {
             prefix = parent == null ? prefix : prefix + node.As + "__";
 
-            var properties = new Properties();
+            var properties = new Dictionary<string, IProperty>();
 
             var sortKey = node.SortKey ?? node.Junction?.SortKey;
             if (sortKey != null)
             {
                 do
                 {
-                    properties.Add(new Property(sortKey.Column)
+                    properties[sortKey.Column] = new Property(sortKey.Column)
                     {
                         Column = $"{prefix}{sortKey.As}",
-                    });
+                    };
                 } while ((sortKey = sortKey.ThenBy) != null);
             }
 
@@ -46,13 +48,21 @@ namespace JoinMonster
                 switch (child)
                 {
                     case SqlColumnBase sqlColumn:
-                        properties.Add(new Property(sqlColumn.FieldName)
+                        if (properties.TryGetValue(sqlColumn.FieldName, out var existing) && existing is Property ep)
                         {
-                            Column = $"{prefix}{sqlColumn.As}",
-                            IsId = sqlColumn.IsId
-                        });
+                            ep.IsId |= sqlColumn.IsId;
+                        }
+                        else
+                        {
+                            properties[sqlColumn.FieldName] = new Property(sqlColumn.FieldName)
+                            {
+                                Column = $"{prefix}{sqlColumn.As}",
+                                IsId = sqlColumn.IsId,
+                            };
+                        }
                         break;
                     case SqlTable sqlTable:
+                        if (properties.ContainsKey(sqlTable.FieldName)) continue;
                         if (sqlTable.Batch == null)
                         {
                             var childProperties = DefineObjectShape(node, prefix, sqlTable);
@@ -62,13 +72,11 @@ namespace JoinMonster
                             else
                                 property = new PropertyObject(sqlTable.FieldName, childProperties);
 
-                            properties.Add(property);
+                            properties[sqlTable.FieldName] = property;
                         }
                         else
                         {
-                            IProperty property = new Property(sqlTable.Batch.ParentKey.FieldName, $"{prefix}{sqlTable.Batch.ParentKey.As}");
-
-                            properties.Add(property);
+                            properties[sqlTable.FieldName] = new Property(sqlTable.Batch.ParentKey.FieldName, $"{prefix}{sqlTable.Batch.ParentKey.As}");;
                         }
 
                         break;
@@ -81,7 +89,7 @@ namespace JoinMonster
                 }
             }
 
-            return properties;
+            return new Properties(properties.Values.ToList());
         }
     }
 }
